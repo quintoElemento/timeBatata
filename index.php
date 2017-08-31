d0bc392c709b538de1d6f9691870daa01baf6182
Your Name <you@example.com>
2017-08-31 13:45:02 -0300

    Table adicionada

--- index.php
+++ index.php
@@ -3,6 +3,23 @@
 	<h1>Aqui é h1!</h1>
 	<h2>Aqui é h2!</h2>
 	<h3>Aqui é h3!</h3>
+	<table>
+	<td>
+	<tr>
+Trrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr1 - Row 1
+	</tr>
+	<tr>
+Trrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr2 - Row 1
+	</tr>
+	</td>
+	<tr>
+Trrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr1 - Row 2
+	</tr>
+	<tr>
+Trrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr2 - Row 2
+	</tr>
+	</td>
+	</table>
 </body>
 </html>
 <?php echo 'Olá, time Batata!'; ?>
